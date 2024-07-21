11cca5c93a1528ca05b95e189ba33bf0af92b027
seung365 <bdh3659@naver.com>
2024-07-21 19:23:38 +0900

    feat: index에 차크라 provider 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "@/App"
+import { ChakraProvider } from "@chakra-ui/react"
+import { theme } from "@/styles/variants/index"
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration"
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
-);
+)
 
-serviceWorkerRegistration.register();
+serviceWorkerRegistration.register()
