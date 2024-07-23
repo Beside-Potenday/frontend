bbda6655158c27e31af3f6a6b0eac3d1079b94da
seung365 <bdh3659@naver.com>
2024-07-24 00:53:54 +0900

    feat: header 스크롤 위치 변경

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,32 +1,38 @@
-import styled from "@emotion/styled"
-import { Link } from "react-router-dom"
-import { Button } from "@chakra-ui/react"
-import { breakpoints } from "@/styles/variants"
+import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
+import { Button } from '@chakra-ui/react';
+import { breakpoints } from '@/styles/variants';
 
 const scrollToSection = (sectionId: string) => {
-  document.getElementById(sectionId)?.scrollIntoView({ behavior: "smooth" })
-}
+  const element = document.getElementById(sectionId);
+  if (element) {
+    const elementPosition = element.getBoundingClientRect().top + window.scrollY - 80;
+    window.scrollTo({ top: elementPosition, behavior: 'smooth' });
+  }
+};
 
 export const Header = () => {
   return (
     <Wrapper>
       <Container>
-        <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
-          <Link to={"/"}>
+        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+          <Link to={'/'}>
             <Logo src="/images/logo.svg" />
           </Link>
-          <MidWrapper onClick={() => scrollToSection("section2")}> 시스템 체험</MidWrapper>
-          <MidWrapper onClick={() => scrollToSection("section3")}> 기능 살펴보기</MidWrapper>
+          <div style={{ display: 'flex', flexDirection: 'row', marginLeft: '47px' }}>
+            <MidWrapper onClick={() => scrollToSection('section2')}> 시스템 체험</MidWrapper>
+            <MidWrapper onClick={() => scrollToSection('section3')}> 기능 살펴보기</MidWrapper>
+          </div>
         </div>
-        <Link to={"/mail"} style={{ display: "flex", alignItems: "center" }}>
+        <Link to={'/mail'} style={{ display: 'flex', alignItems: 'center' }}>
           <AiButton>AI 메일 생성하기</AiButton>
         </Link>
       </Container>
     </Wrapper>
-  )
-}
+  );
+};
 
-export const HEADER_HEIGHT = "80px"
+export const HEADER_HEIGHT = '80px';
 
 const Wrapper = styled.header`
   z-index: 100;
@@ -37,20 +43,21 @@ const Wrapper = styled.header`
   left: 0;
   right: 0;
   width: 100%;
-  background: transparent;
-`
+  background: linear-gradient(90deg, #e6e9ff 0%, #e5f3ff 100%);
+  box-shadow: 0px 2px 18px 2px rgba(0, 0, 0, 0.1);
+`;
 
 const Logo = styled.img`
   height: 48px;
   cursor: pointer;
-`
+`;
 
 const Container = styled.div`
   display: flex;
   flex-direction: row;
   font-size: 14px;
   justify-content: space-between;
-`
+`;
 
 const AiButton = styled(Button)`
   height: 20px;
@@ -58,11 +65,11 @@ const AiButton = styled(Button)`
   padding: 10px 20px;
   border-radius: 20px;
   border: 1px solid var(--black, #6ab9f2);
-`
+`;
 const MidWrapper = styled.div`
   cursor: pointer;
   margin: 0px 20px;
   @media (max-width: ${breakpoints.md}) {
     display: none;
   }
-`
+`;
