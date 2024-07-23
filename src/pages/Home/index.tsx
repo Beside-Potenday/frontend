df3b3770f02e486774fc3855c0f82f3c69d574b8
seung365 <bdh3659@naver.com>
2024-07-24 00:46:11 +0900

    feat: 백그라운드 컬러 수정

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,18 @@
 import { Contents } from "@/components/HomePage/Contents"
+import styled from "@emotion/styled"
 
 export const HomePage = () => {
   return (
-    <div>
+    <Wrapper>
       <Contents />
-    </div>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  width: 100%;
+  background: radial-gradient(50% 50% at 75% 25%, #d8edfd 0%, rgba(216, 237, 253, 0) 100%),
+    radial-gradient(50% 50% at 25% 25%, #dee2fd 0%, rgba(230, 233, 255, 0) 100%),
+    linear-gradient(180deg, #e5efff 0%, #fff 100%);
+  overflow-x: hidden;
+`
