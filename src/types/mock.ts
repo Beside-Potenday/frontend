adaaa36165d7c7a9ca7d88c145f3ebc65685b0e7
seung365 <bdh3659@naver.com>
2024-07-24 00:44:46 +0900

    chore: mock 데이터 추가

---
+++ src/types/mock.ts
@@ -0,0 +1,64 @@
+export const mockData = [
+  {
+    mailPurpose: '질문',
+    senderDepartment: '컴퓨터공학과',
+    senderId: '2020123456',
+    courseName: '자료구조',
+  },
+  {
+    mailPurpose: '과제 제출',
+    senderDepartment: '전자공학과',
+    senderId: '2020234567',
+    courseName: '회로이론',
+  },
+  {
+    mailPurpose: '성적 정정',
+    senderDepartment: '기계공학과',
+    senderId: '2020345678',
+    courseName: '열역학',
+  },
+  {
+    mailPurpose: '병결 요청',
+    senderDepartment: '산업공학과',
+    senderId: '2020456789',
+    courseName: '운영관리',
+  },
+  {
+    mailPurpose: '상담 요청',
+    senderDepartment: '화학공학과',
+    senderId: '2020567890',
+    courseName: '화학반응공학',
+  },
+  {
+    mailPurpose: '질문',
+    senderDepartment: '건축학과',
+    senderId: '2020678901',
+    courseName: '건축설계',
+  },
+  {
+    mailPurpose: '과제 제출',
+    senderDepartment: '토목공학과',
+    senderId: '2020789012',
+    courseName: '구조역학',
+  },
+  {
+    mailPurpose: '성적 정정',
+    senderDepartment: '컴퓨터공학과',
+    senderId: '2020890123',
+    courseName: '알고리즘',
+  },
+  {
+    mailPurpose: '병결 요청',
+    senderDepartment: '전자공학과',
+    senderId: '2020901234',
+    courseName: '전자기학',
+  },
+  {
+    mailPurpose: '상담 요청',
+    senderDepartment: '기계공학과',
+    senderId: '2021012345',
+    courseName: '기계진동',
+  },
+];
+
+export default mockData;
