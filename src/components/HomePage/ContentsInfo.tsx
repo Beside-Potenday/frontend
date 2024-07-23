32cd5ed5c0d01023d1f1f10f5fa5311cd3b6a099
kiyeong <kjr07145@gmail.com>
2024-07-23 17:36:09 +0900

    feat: 살펴보기 로고 추가

--- src/components/HomePage/ContentsInfo.tsx
+++ src/components/HomePage/ContentsInfo.tsx
@@ -1,10 +1,10 @@
-import { Grid, GridItem, Box, Text } from "@chakra-ui/react"
-import styled from "@emotion/styled"
+import { Img, Grid, GridItem, Box, Text } from '@chakra-ui/react';
+import styled from '@emotion/styled';
 
 export const ContentsInfo = () => {
   return (
     <Wrapper>
-      <Title>살펴보기</Title>
+      <Img src="/images/contentsInfoLogo.svg"></Img>
       <Grid templateRows="repeat(1, 1fr)" templateColumns="repeat(3, 1fr)" gap={6}>
         {cardContents.map((content, index) => (
           <GridItem key={index} as={Card}>
@@ -18,18 +18,12 @@ export const ContentsInfo = () => {
         ))}
       </Grid>
     </Wrapper>
-  )
-}
+  );
+};
 
 const Wrapper = styled.div`
   padding: 50px;
-`
-
-const Title = styled.h1`
-  font-size: 24px;
-  font-weight: bold;
-  margin-bottom: 20px;
-`
+`;
 
 const Card = styled.div`
   background: white;
@@ -37,25 +31,25 @@ const Card = styled.div`
   border-radius: 8px;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
   text-align: center;
-`
+`;
 
 const cardContents = [
   {
-    title: "메일 생성 준비 단 30초!",
+    title: '메일 생성 준비 단 30초!',
     image: <img src="/path/to/image1.png" alt="gif파일1" />,
-    description1: "간단한 필수 정보만 입력하면 준비 완료!",
-    description2: "사용자 유형에 따라 다른 양식을 제공해요",
+    description1: '간단한 필수 정보만 입력하면 준비 완료!',
+    description2: '사용자 유형에 따라 다른 양식을 제공해요',
   },
   {
-    title: "AI 메일 생성",
+    title: 'AI 메일 생성',
     image: <img src="/path/to/image2.png" alt="gif파일2" />,
-    description1: "나에게 딱 필요한 메일을 빠르고 정확하게!",
-    description2: "입력한 정보를 기반으로 맞춤형 메일을 생성해요.",
+    description1: '나에게 딱 필요한 메일을 빠르고 정확하게!',
+    description2: '입력한 정보를 기반으로 맞춤형 메일을 생성해요.',
   },
   {
-    title: "생성한 메일 전송",
+    title: '생성한 메일 전송',
     image: <img src="/path/to/image3.png" alt="gif파일3" />,
-    description1: "생성된 메일은 간편하게 복붙하거나",
-    description2: "바로 전송하며 신속하게 해결해요!",
+    description1: '생성된 메일은 간편하게 복붙하거나',
+    description2: '바로 전송하며 신속하게 해결해요!',
   },
-]
+];
