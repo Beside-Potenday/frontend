f1fbdb7da00cf37dde95d53eb7dbde08bf4bede4
kiyeong <kjr07145@gmail.com>
2024-07-24 19:29:09 +0900

    feat: 페이지 별 Footer 유무 수정

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { HomePage } from '@/pages/Home';
 import { MailPage } from '@/pages/Mail';
 import { Layout } from '@/components/Layout';
+import { NoFooterLayout } from '@/components/Layout/noFooterIndex';
 import { RouterPath } from './path';
 import Terms from '@/pages/Extra/Terms';
 import Privacy from '@/pages/Extra/Privacy';
@@ -11,8 +12,12 @@ const router = createBrowserRouter([
   {
     path: RouterPath.root,
     element: <Layout />,
+    children: [{ path: RouterPath.home, element: <HomePage /> }],
+  },
+  {
+    path: RouterPath.root,
+    element: <NoFooterLayout />,
     children: [
-      { path: RouterPath.home, element: <HomePage /> },
       { path: RouterPath.mail, element: <MailPage /> },
       { path: RouterPath.terms, element: <Terms /> },
       { path: RouterPath.privacy, element: <Privacy /> },
