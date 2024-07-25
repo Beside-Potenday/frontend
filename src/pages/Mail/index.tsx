e014ca1ee3e097ed394c88cc15d8c105d5973ba4
kiyeong <kjr07145@gmail.com>
2024-07-25 17:38:14 +0900

    feat: 메일생성하기 이미지 크기 수정

--- src/pages/Mail/index.tsx
+++ src/pages/Mail/index.tsx
@@ -1,9 +1,8 @@
 import styled from '@emotion/styled';
-import { Grid, GridItem } from '@chakra-ui/react';
+import { Grid, GridItem, Box, Flex } from '@chakra-ui/react';
 import { Header } from '@/components/Mail/Header';
 import { useState } from 'react';
 import { AskList } from '@/components/Mail/AskList';
-
 import { useMail } from '@/Provider/MailContext';
 
 export const MailPage = () => {
@@ -25,65 +24,95 @@ export const MailPage = () => {
     <Wrapper>
       <LogoWrapper>
         <ContentWrapper>
-          <Grid
-            w="100%"
-            h="100%"
-            templateRows="106px 80px 582px"
-            templateColumns={{ base: 'repeat(1, 1fr)' }}
-          >
-            <GridItem rowSpan={1}></GridItem>
-            <GridItem
-              rowSpan={1}
-              background={'white'}
-              display="flex"
-              flexDirection="column"
-              alignItems="center"
-              justifyContent="center"
-              position="relative"
-            >
+          <StyledGrid>
+            <StyledGridItem className="empty"></StyledGridItem>
+            <StyledGridItem className="header">
               <Header isActive={isActive} onIsActive={onIsActive}></Header>
-            </GridItem>
-            <GridItem rowSpan={1}>
-              <Grid
-                w="100%"
-                h="100%"
-                templateRows={{ base: 'repeat(1, 1fr)' }}
-                templateColumns={{ base: 'repeat(3, 1fr)' }}
-              >
-                <GridItem colSpan={2}>
+            </StyledGridItem>
+            <StyledGridItem className="content">
+              <InnerGrid>
+                <InnerGridItem className="ask-list">
                   <AskList randomInput={mailInput}></AskList>
-                </GridItem>
-                <GridItem
-                  colSpan={1}
-                  display={'flex'}
-                  justifyContent={'space-between'}
-                  alignItems={'flex-end'}
-                  flexDirection={'column'}
-                  marginRight="56px"
-                  marginBottom="25px"
-                ></GridItem>
-              </Grid>
-            </GridItem>
-          </Grid>
+                </InnerGridItem>
+                <InnerGridItem className="image-column">
+                  <img src="/images/mailEnter.svg" alt="Enter Key" />
+                  <img src="/images/mailSkip.svg" alt="Skip" />
+                  <img src="/images/mailUse.svg" alt="Info" />
+                </InnerGridItem>
+              </InnerGrid>
+            </StyledGridItem>
+          </StyledGrid>
         </ContentWrapper>
       </LogoWrapper>
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div`
+const Wrapper = styled(Flex)`
   height: auto;
-  display: flex;
   align-items: center;
   justify-content: center;
 `;
 
-const LogoWrapper = styled.div`
+const LogoWrapper = styled(Box)`
   width: 100%;
   max-width: 1200px;
 `;
-const ContentWrapper = styled.div`
+
+const ContentWrapper = styled(Box)`
   width: 100%;
   height: 768px;
   background: #fff;
+`;
+
+const StyledGrid = styled(Grid)`
+  width: 100%;
+  height: 100%;
+  grid-template-rows: 106px 80px 582px;
+  grid-template-columns: repeat(1, 1fr);
+`;
+
+const StyledGridItem = styled(GridItem)`
+  &.empty {
+    grid-row: span 1;
+  }
+
+  &.header {
+    grid-row: span 1;
+    background: white;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+  }
+
+  &.content {
+    grid-row: span 1;
+  }
+`;
+
+const InnerGrid = styled(Grid)`
+  width: 100%;
+  height: 100%;
+  grid-template-rows: repeat(1, 1fr);
+  grid-template-columns: repeat(3, 1fr);
+`;
+
+const InnerGridItem = styled(GridItem)`
+  &.ask-list {
+    grid-column: span 2;
+  }
+
+  &.image-column {
+    grid-column: span 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: flex-end;
+    margin-right: 56px;
+    margin-bottom: 25px;
+    margin-top: 25px;
+    margin-left: -70px;
+  }
 `;
