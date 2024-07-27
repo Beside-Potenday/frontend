50df229b0d2d3f0d9ad15739959fac756cfa6353
seung365 <bdh3659@naver.com>
2024-07-28 00:31:53 +0900

    fix: 재시도

--- src/api/hooks/usePostUniv.tsx
+++ src/api/hooks/usePostUniv.tsx
@@ -2,7 +2,7 @@ import { useMutation } from '@tanstack/react-query';
 import { mailSend, mailResponseData } from '@/types';
 import { BASE_URL } from '..';
 import axios from 'axios';
-export const postUnivPath = () => `${BASE_URL}/mails/univ`;
+export const postUnivPath = () => `${BASE_URL}/api/mails/univ`;
 
 export const postUniv = async (mailData: mailSend): Promise<mailResponseData> => {
   const response = await axios.post(postUnivPath(), mailData);
