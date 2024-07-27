2ffc2dda9b8dcd9a4bee18ae54b57d6966ac07f9
seung365 <bdh3659@naver.com>
2024-07-28 00:34:50 +0900

    fix: 재시도

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import { QueryClient } from '@tanstack/react-query';
 
-export const BASE_URL = 'http://3.34.138.24:8080';
+export const BASE_URL = 'http://3.34.138.24';
 
 export const queryClient = new QueryClient({
   defaultOptions: {
