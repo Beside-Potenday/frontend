284696de2ce0d32ce954671f9d3b393e41da6086
seung365 <bdh3659@naver.com>
2024-07-28 09:53:36 +0900

    fix: 재시도

--- src/api/hooks/usePostUniv.tsx
+++ src/api/hooks/usePostUniv.tsx
@@ -2,16 +2,11 @@ import { useMutation } from '@tanstack/react-query';
 import { mailSend, mailResponseData } from '@/types';
 import { BASE_URL } from '..';
 import axios from 'axios';
-import https from 'https';
 
 export const postUnivPath = () => `${BASE_URL}/api/mails/univ`;
 
 export const postUniv = async (mailData: mailSend): Promise<mailResponseData> => {
-  const response = await axios.post(postUnivPath(), mailData, {
-    httpsAgent: new https.Agent({
-      rejectUnauthorized: false, // SSL 인증서 검증 비활성화 (주의!)
-    }),
-  });
+  const response = await axios.post(postUnivPath(), mailData);
   return response.data;
 };
 
