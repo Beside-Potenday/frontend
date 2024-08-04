f7195fb7353d512a166be58b62b07072f8e52cad
seung365 <bdh3659@naver.com>
2024-08-05 02:52:05 +0900

    feat: UpperImage 추가

---
+++ src/components/Layout/UpperImage.tsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const UpperImage = () => {
+  return <Wrapper> 어퍼 이미지</Wrapper>;
+};
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 400px;
+  background-image: url('/images/upper_image.jpg');
+`;
