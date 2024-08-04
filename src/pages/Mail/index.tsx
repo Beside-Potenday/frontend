7b61665a54a05af718e380289350149141228d77
seung365 <bdh3659@naver.com>
2024-08-05 02:51:29 +0900

    feat: mail page의 배경화면 및 레이아웃 수정

--- src/pages/Mail/index.tsx
+++ src/pages/Mail/index.tsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
-import { Grid, GridItem, Box, Flex } from '@chakra-ui/react';
+import { Grid, GridItem, Box } from '@chakra-ui/react';
 import { Header } from '@/components/Mail/Header';
 import { AskList } from '@/components/Mail/AskList';
 import { useMail } from '@/Provider/MailContext';
 import { MailModal } from '@/components/Mail/MailModal';
+import { breakpoints } from '@/styles/variants';
 
 export const MailPage = () => {
   const [isModalOpen, setIsModalOpen] = useState(true); // 모달을 처음에 열리게 설정
@@ -19,7 +20,6 @@ export const MailPage = () => {
     setIsModalOpen(false);
   };
 
-  // useEffect를 사용하여 컴포넌트가 마운트될 때 모달을 열기
   useEffect(() => {
     setIsModalOpen(true);
   }, []);
@@ -28,24 +28,40 @@ export const MailPage = () => {
     <Wrapper>
       <LogoWrapper>
         <ContentWrapper>
-          <StyledGrid>
-            <StyledGridItem className="empty"></StyledGridItem>
-            <StyledGridItem className="header">
-              <Header isActive={mailContext.isActive} onIsActive={mailContext.onIsActive}></Header>
-            </StyledGridItem>
-            <StyledGridItem className="content">
-              <InnerGrid>
-                <InnerGridItem className="ask-list">
+          <Grid
+            w="100%"
+            h="100%"
+            templateRows={{ base: '106px 780px' }}
+            templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(1, 1fr)' }}
+          >
+            <MedeaItems
+              rowSpan={1}
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+              justifyContent="center"
+              position="relative"
+            >
+              <Header isActive={mailContext.isActive} onIsActive={mailContext.onIsActive} />
+            </MedeaItems>
+
+            <GridItem colSpan={1}>
+              <Grid templateColumns="repeat(3, 1fr)" h="100%">
+                <GridItem colSpan={{ base: 2 }}>
                   <AskList randomInput={mailInput}></AskList>
-                </InnerGridItem>
-                <InnerGridItem className="image-column">
-                  <img src="/images/mailEnter.svg" alt="Enter Key" />
-                  <img src="/images/mailSkip.svg" alt="Skip" />
-                  <img src="/images/mailUse.svg" alt="Info" />
-                </InnerGridItem>
-              </InnerGrid>
-            </StyledGridItem>
-          </StyledGrid>
+                </GridItem>
+                <GridItem
+                  colSpan={1}
+                  display={'flex'}
+                  justifyContent={{ base: 'center', md: 'space-between' }}
+                  alignItems={{ base: 'center', md: 'flex-end' }}
+                  flexDirection={{ base: 'row', md: 'column' }}
+                >
+                  <ImageWrapper src="/images/exampleImage.svg" />
+                </GridItem>
+              </Grid>
+            </GridItem>
+          </Grid>
         </ContentWrapper>
       </LogoWrapper>
       <AnimatedMailModal isOpen={isModalOpen} onClose={closeModal} />
@@ -71,70 +87,33 @@ const AnimatedMailModal = styled(MailModal)`
   animation: fadeIn 0.5s ease-out forwards;
 `;
 
-const Wrapper = styled(Flex)`
-  height: auto;
+const Wrapper = styled.div`
+  display: flex;
   align-items: center;
   justify-content: center;
+  background: var(--Blue100, #e5f3ff);
 `;
 
-const LogoWrapper = styled(Box)`
+const LogoWrapper = styled.div`
   width: 100%;
   max-width: 1200px;
+  background: radial-gradient(
+    50% 50% at 50% 50%,
+    rgba(0, 148, 255, 0.2) 0%,
+    rgba(0, 148, 255, 0) 100%
+  );
+  border-radius: 1244px;
 `;
 
 const ContentWrapper = styled(Box)`
   width: 100%;
-  height: 768px;
-  background: #fff;
-`;
-
-const StyledGrid = styled(Grid)`
-  width: 100%;
   height: 100%;
-  grid-template-rows: 106px 80px 582px;
-  grid-template-columns: repeat(1, 1fr);
 `;
 
-const StyledGridItem = styled(GridItem)`
-  &.empty {
-    grid-row: span 1;
-  }
-
-  &.header {
-    grid-row: span 1;
-    background: white;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    position: relative;
-  }
-
-  &.content {
-    grid-row: span 1;
+const MedeaItems = styled(Grid)`
+  @media (${breakpoints.md}) {
+    display: none;
   }
 `;
 
-const InnerGrid = styled(Grid)`
-  width: 100%;
-  height: 100%;
-  grid-template-rows: repeat(1, 1fr);
-  grid-template-columns: repeat(3, 1fr);
-`;
-
-const InnerGridItem = styled(GridItem)`
-  &.ask-list {
-    grid-column: span 2;
-  }
-
-  &.image-column {
-    grid-column: span 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: flex-end;
-    margin-right: 130px;
-    margin-top: 28px;
-    margin-left: -100px;
-  }
-`;
+const ImageWrapper = styled.img``;
