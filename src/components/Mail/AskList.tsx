ece05e8c7d2b7404765e53b08037f48515c83f63
seung365 <bdh3659@naver.com>
2024-08-06 01:07:27 +0900

    feat: mail의 asklist 직장인 버전 추가

--- src/components/Mail/AskList.tsx
+++ src/components/Mail/AskList.tsx
@@ -1,39 +1,42 @@
 import styled from '@emotion/styled';
-import { mailSend } from '@/types';
+import { AskListProps, Question } from '@/types';
+import { useMail } from '@/Provider/MailContext';
+import { useState, useEffect } from 'react';
 
-export const AskList = ({ randomInput }: { randomInput: mailSend }) => {
+export const AskList = ({ randomInput }: AskListProps) => {
+  const { isActive } = useMail();
+  const [questions, setQuestions] = useState<Question[]>([]);
+
+  useEffect(() => {
+    if (isActive === 'business' && 'company' in randomInput) {
+      setQuestions([
+        { ask: '메일 작성 목적을 입력해 주세요', input: randomInput.content },
+        { ask: '보내는 사람의 이름을 입력해 주세요', input: randomInput.sender },
+        { ask: '소속 회사명을 입력해 주세요', input: randomInput.company },
+        { ask: '소속 부서를 입력해 주세요', input: randomInput.department },
+        { ask: '추가 기재사항을 입력해 주세요', input: randomInput.additional },
+        { ask: '받는 사람의 이름을 입력해 주세요', input: randomInput.receiver },
+      ]);
+    } else if (isActive !== 'business' && 'studentId' in randomInput) {
+      setQuestions([
+        { ask: '메일 작성 목적을 입력해 주세요', input: randomInput.content },
+        { ask: '보내는 사람의 이름을 입력해 주세요', input: randomInput.sender },
+        { ask: '보내는 사람의 학과를 입력해 주세요', input: randomInput.department },
+        { ask: '보내는 사람의 학번을 입력해 주세요', input: randomInput.studentId },
+        { ask: '강의명을 입력해 주세요', input: randomInput.subject },
+        { ask: '받는 사람의 이름을 입력해주세요', input: randomInput.receiver },
+      ]);
+    }
+  }, [isActive, randomInput]);
+
   return (
     <AskListWrapper>
-      <AskListItemWrapper>
-        <AskListItem>메일 작성 목적을 입력해 주세요</AskListItem>
-        <InputListItem visible={!!randomInput.content}>{randomInput.content || ' '}</InputListItem>
-      </AskListItemWrapper>
-      <AskListItemWrapper>
-        <AskListItem>보내는 사람의 이름을 입력해 주세요</AskListItem>
-        <InputListItem visible={!!randomInput.sender}>{randomInput.sender || ' '}</InputListItem>
-      </AskListItemWrapper>
-      <AskListItemWrapper>
-        <AskListItem>보내는 사람의 학과를 입력해 주세요</AskListItem>
-        <InputListItem visible={!!randomInput.department}>
-          {randomInput.department || ' '}
-        </InputListItem>
-      </AskListItemWrapper>
-      <AskListItemWrapper>
-        <AskListItem>보내는 사람의 학번을 입력해 주세요</AskListItem>
-        <InputListItem visible={!!randomInput.studentId}>
-          {randomInput.studentId || ' '}
-        </InputListItem>
-      </AskListItemWrapper>
-      <AskListItemWrapper>
-        <AskListItem>강의명을 입력해 주세요</AskListItem>
-        <InputListItem visible={!!randomInput.subject}>{randomInput.subject || ' '}</InputListItem>
-      </AskListItemWrapper>
-      <AskListItemWrapper>
-        <AskListItem>받는 사람의 이름을 입력해주세요</AskListItem>
-        <InputListItem visible={!!randomInput.receiver}>
-          {randomInput.receiver || ' '}
-        </InputListItem>
-      </AskListItemWrapper>
+      {questions.map((question, index) => (
+        <AskListItemWrapper key={index}>
+          <AskListItem>{question.ask}</AskListItem>
+          <InputListItem visible={!!question.input}>{question.input || ' '}</InputListItem>
+        </AskListItemWrapper>
+      ))}
     </AskListWrapper>
   );
 };
