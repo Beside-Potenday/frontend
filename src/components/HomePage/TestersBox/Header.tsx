1ce5e472f985a3c9e3fe2d3ac178606b1c28d7b9
seung365 <bdh3659@naver.com>
2024-08-06 01:09:19 +0900

    feat: 홈페이지 체험하기 헤더 레이아웃 수정+ 직장인 선택 추가

--- src/components/HomePage/TestersBox/Header.tsx
+++ src/components/HomePage/TestersBox/Header.tsx
@@ -1,90 +1,64 @@
 import styled from '@emotion/styled';
-import { breakpoints } from '@/styles/variants'; // breakpoints를 가져옵니다.
 
 interface HeaderProps {
   isActive: string;
-  onIsActive: (isActive: string) => void;
+  onIsActive: (state: 'univ' | 'business') => void;
 }
 
 interface StudentProps {
   isActive: string;
 }
 
+interface BusinessProps {
+  isActive: string;
+}
+
 export const Header = ({ isActive, onIsActive }: HeaderProps) => {
   return (
-    <HeaderWrapper>
-      <Student isActive={isActive} onClick={() => onIsActive('univ')}>
-        대학생
-      </Student>
-      <Office>
-        직장인
-        <HoverImage src="/images/hoveroffice.svg" alt="Hover Image" />
-      </Office>
+    <>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'flex-start',
+          width: '100%',
+          paddingTop: '33px',
+        }}
+      >
+        <Student isActive={isActive} onClick={() => onIsActive('univ')}>
+          대학생
+        </Student>
+        <Business isActive={isActive} onClick={() => onIsActive('business')}>
+          직장인
+        </Business>
+      </div>
       <Bar></Bar>
-    </HeaderWrapper>
+    </>
   );
 };
-
-const HeaderWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: flex-start;
-  width: 100%;
-  padding-top: 33px;
-  position: relative;
-`;
 
 const Student = styled.div<StudentProps>`
   margin-left: 167px;
   cursor: pointer;
-  box-shadow: ${(props) => (props.isActive ? '0 4px 0 0 #6AB9F2' : 'none')};
+  box-shadow: ${(props) => (props.isActive === 'univ' ? '0 4px 0 0 #6AB9F2' : 'none')};
   position: relative;
   z-index: 2;
-  bottom: -1px;
-
-  @media (${breakpoints.md}) {
-    margin-left: 20px;
-  }
+  bottom: -13px;
 `;
 
-const Office = styled.div`
+const Business = styled.div<BusinessProps>`
   margin-left: 44px;
+  cursor: pointer;
+  box-shadow: ${(props) => (props.isActive === 'business' ? '0 4px 0 0 #6AB9F2' : 'none')};
   position: relative;
   z-index: 2;
-  bottom: -1px;
-
-  &:hover > img {
-    visibility: visible;
-  }
-
-  @media (${breakpoints.md}) {
-    margin-left: 20px;
-  }
-`;
-
-const HoverImage = styled.img`
-  visibility: hidden;
-  position: relative;
-  top: -70px;
-  left: 50%;
-  transform: translateX(-50%);
-  width: 174px;
-  height: 34px;
-  flex-shrink: 0;
-  z-index: 3;
+  bottom: -13px;
 `;
 
 const Bar = styled.div`
   background: white;
-  width: 1300px;
+  width: 1200px;
   height: 3px;
   position: absolute;
-  bottom: 45px;
-  z-index: 1; /* Ensure it is below Student and Office */
-
-  @media (${breakpoints.md}) {
-    position: relative;
-    bottom: -20px;
-    margin-top: 10px;
-  }
+  bottom: 8px;
 `;
