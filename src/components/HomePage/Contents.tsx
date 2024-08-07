813f1496422858cd55cda533d87a065e547a9e01
seung365 <bdh3659@naver.com>
2024-08-07 15:51:51 +0900

    design: 홈페이지 디자인 수정

--- src/components/HomePage/Contents.tsx
+++ src/components/HomePage/Contents.tsx
@@ -1,21 +1,24 @@
 import { Grid, GridItem } from '@chakra-ui/react';
 import { ContentsInfo } from '@/components/HomePage/ContentsInfo';
 import { TestersBox } from './TestersBox';
-import { TopContents } from './TopContents';
 
 export const Contents = () => {
   return (
     <Grid
-      h={{ base: '2000px', md: '2500px' }} // 'md' 이상일 때 2500px, 작을 때 2000px
+      h={{ base: '1500px', md: '2000px' }} // 'md' 이상일 때 2500px, 작을 때 2000px
       w="100%"
-      templateRows={{ base: 'repeat(3, 1fr)', md: '1fr 1fr 600px' }} // 'md' 이상일 때 '1fr 1fr 600px', 작을 때 'repeat(3, 1fr)'
-      templateColumns={{ base: '1fr', md: 'repeat(1, 1fr)' }} // 'md' 이상일 때 'repeat(1, 1fr)', 작을 때 '1fr'
+      templateRows={{ base: 'auto auto', md: ' 1fr 600px' }} // 'md' 이상일 때 '1fr 1fr 600px', 작을 때 'repeat(3, 1fr)'
+      templateColumns={{ base: 'auto', md: 'repeat(1, 1fr)' }} // 'md' 이상일 때 'repeat(1, 1fr)', 작을 때 '1fr'
       gap={{ base: 50, md: 100 }} // 'md' 이상일 때 100, 작을 때 50
     >
-      <GridItem id="section1" rowSpan={1} backgroundColor={'transparent'}>
-        <TopContents />
-      </GridItem>
-      <GridItem id="section2" rowSpan={1} backgroundColor={'transparent'}>
+      <GridItem
+        id="section2"
+        rowSpan={1}
+        backgroundColor={'transparent'}
+        display="flex"
+        justifyContent={'center'}
+        gap={10}
+      >
         <TestersBox />
       </GridItem>
       <GridItem id="section3" rowSpan={1} backgroundColor={'transparent'}>
