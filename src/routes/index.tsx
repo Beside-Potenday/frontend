2808ac1537f7c2ff72d6ee04b3b3008fc6d13958
seung365 <bdh3659@naver.com>
2024-08-07 15:52:55 +0900

    feat: routes 수정

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,15 +4,25 @@ import { MailPage } from '@/pages/Mail';
 import { Layout } from '@/components/Layout';
 import { NoFooterLayout } from '@/components/Layout/noFooterIndex';
 import { RouterPath } from './path';
-import {Terms} from '@/pages/Extra/Terms';
-import {Privacy} from '@/pages/Extra/Privacy';
-import {Contact} from '@/pages/Extra/Contact';
+import { Terms } from '@/pages/Extra/Terms';
+import { Privacy } from '@/pages/Extra/Privacy';
+import { Contact } from '@/pages/Extra/Contact';
+import { Login } from '@/pages/Login';
+import { MyPage } from '@/pages/MyPage';
+import { PrivateRoute } from './components/PrivateRoute';
+import { Navigate } from 'react-router-dom';
 
 const router = createBrowserRouter([
   {
     path: RouterPath.root,
     element: <Layout />,
-    children: [{ path: RouterPath.home, element: <HomePage /> }],
+    children: [
+      { path: RouterPath.home, element: <HomePage /> },
+      {
+        path: RouterPath.notFound,
+        element: <Navigate to={RouterPath.home} />,
+      },
+    ],
   },
   {
     path: RouterPath.root,
@@ -22,7 +32,16 @@ const router = createBrowserRouter([
       { path: RouterPath.terms, element: <Terms /> },
       { path: RouterPath.privacy, element: <Privacy /> },
       { path: RouterPath.contact, element: <Contact /> },
+      {
+        path: RouterPath.mypage,
+        element: <PrivateRoute />,
+        children: [{ path: RouterPath.mypage, element: <MyPage /> }],
+      },
     ],
+  },
+  {
+    path: RouterPath.login,
+    element: <Login />,
   },
 ]);
 
